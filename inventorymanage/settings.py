a407aad102c02b2e57677b664d6a88f19b6f98c3
Lokesh Kad <115694701+lokesh-6@users.noreply.github.com>
2024-01-25 22:29:23 +0530

    Update settings.py

--- inventorymanage/settings.py
+++ inventorymanage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i261e1bl%zty54m0!q&2w&zbc97n4cda#$w@pc+#@uzsea^%o4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
