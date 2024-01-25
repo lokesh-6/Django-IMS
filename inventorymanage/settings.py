fe0bb63b976eebb74abb7350fb1193312d1d919c
Lokesh Kad <115694701+lokesh-6@users.noreply.github.com>
2024-01-26 00:05:31 +0530

    Update settings.py

--- inventorymanage/settings.py
+++ inventorymanage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i261e1bl%zty54m0!q&2w&zbc97n4cda#$w@pc+#@uzsea^%o4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
