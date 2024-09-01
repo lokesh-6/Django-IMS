34d48f055ab113fa858205ec327613fba816af5d
lokesh-6 <kadlokesh@gmail.com>
2024-09-02 00:34:27 +0530

    resolved

--- inventorymanage/settings.py
+++ inventorymanage/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,7 +83,7 @@ WSGI_APPLICATION = 'inventorymanage.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
